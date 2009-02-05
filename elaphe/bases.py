de42c1140abf295dfd3c4d8eb836ac55f8a23f18
whosaysni <whosaysni@gmail.com>
2009-02-05 04:25:34 +0000

    Splitted doctest.

--- elaphe/bases.py
+++ elaphe/bases.py
@@ -11,36 +11,34 @@ import config, utils
 
 class BarcodeRenderer(object):
     """
-    >>> br = BarcodeRenderer()
-    >>> print br.render_ps_code('977147396801', (-20, -20, 92, 164)) # doctest: +ELLIPSIS
-    %!PS-Adobe-2.0
-    %%Pages: (attend)
-    %%Creator: Elaphe powered by barcode.ps
-    %%BoundingBox: -20 -20 92 164
-    %%LanguageLevel: 2
-    %%EndComments
-    <BLANKLINE>
-    <BLANKLINE>
-    <BLANKLINE>
-    % --BEGIN ENCODER ean13--
-    ...
-    % --END DISPATCHER--
-    <BLANKLINE>
-    <BLANKLINE>
-    <BLANKLINE>
-    gsave
-    0 0 moveto
-    (977147396801) ean13 barcode
-    grestore
-    showpage
-    <BLANKLINE>
-    >>> im = br.render('977147396801')
-    >>> im # doctest: +ELLIPSIS
-    <PIL.EpsImagePlugin.EpsImageFile instance at ...>
     """
     codetype = 'ean13'
 
     def render_ps_code(self, codestring, bbox=(-20, -20, 164, 92), options=None, **kw):
+        """
+        >>> print BarcodeRenderer().render_ps_code('977147396801', (-20, -20, 92, 164)) # doctest: +ELLIPSIS
+        %!PS-Adobe-2.0
+        %%Pages: (attend)
+        %%Creator: Elaphe powered by barcode.ps
+        %%BoundingBox: -20 -20 92 164
+        %%LanguageLevel: 2
+        %%EndComments
+        <BLANKLINE>
+        <BLANKLINE>
+        <BLANKLINE>
+        % --BEGIN ENCODER ean13--
+        ...
+        % --END DISPATCHER--
+        <BLANKLINE>
+        <BLANKLINE>
+        <BLANKLINE>
+        gsave
+        0 0 moveto
+        (977147396801) ean13 barcode
+        grestore
+        showpage
+        <BLANKLINE>
+        """
         params = {}
         params['bbox'] = "%d %d %d %d" %bbox
         params['codestring'] = utils.ps_string(codestring)
@@ -50,8 +48,13 @@ class BarcodeRenderer(object):
         return config.PS_CODE_TEMPLATE %(params)
 
     def render(self, codestring, options=None, **kw):
+        """
+        >>> BarcodeRenderer().render('977147396801') # doctest: +ELLIPSIS
+        <PIL.EpsImagePlugin.EpsImageFile instance at ...>
+        """
         ps_code_buf = self.render_ps_code(codestring, options=options, **kw)
         return EpsImageFile(StringIO.StringIO(ps_code_buf))
+
 
 if __name__=="__main__":
     from doctest import testmod
