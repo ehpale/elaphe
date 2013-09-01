7bc8225e3196777b4f276c680d3fa0cfc01e2783
Yasushi Masuda <whosaysni@gmail.com>
2013-09-02 08:49:07 +0900

    fixed setup.py to work on setuptools>0.6

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ import sys, os
 from os.path import abspath, dirname, join as pathjoin
 from setuptools import setup
 
-sys.path.insert(0, abspath('./tests'))
-sys.path.insert(0, abspath('./elaphe'))
+from elaphe import __version__
 
-import elaphe
-
-version = '.'.join(map(str, elaphe.__version__))
-
-install_requires = ['setuptools', 'Pillow']
+version = '.'.join(map(str, __version__))
+install_requires = [] # 'setuptools', 'Pillow']
 test_requires = []
 extra_requires = {}
 long_description = '\n'.join([
@@ -21,10 +17,12 @@ setup_params = dict(
     name="elaphe",
     version=version,
     packages=['elaphe'],
-    exclude_package_data={'elaphe': ['postscriptbarcode']},
-    package_data={'elaphe': ['postscriptbarcode/barcode.ps', 'postscriptbarcode/LICENSE']},
+    exclude_package_data={
+        'elaphe': ['postscriptbarcode']},
+    package_data={
+        'elaphe': ['postscriptbarcode/barcode.ps',
+                   'postscriptbarcode/LICENSE']},
     zip_safe=False,
-    dependency_links = ["http://dist.repoze.org"], #PIL as egg
     install_requires = install_requires,
     tests_require=test_requires,
     extras_require=extra_requires,
@@ -43,10 +41,9 @@ setup_params = dict(
     long_description=long_description,
     license = "New BSD",
     keywords = "barcode convert postscript image graphics",
-    url = "http://elaphe.googlecode.com",
-    download_url = "http://code.google.com/p/elaphe/downloads/list",
-    # entry_points = {'console_scripts': ['elaphe = elaphe.:main']},
+    url = "http://bitbucket.org/whosaysni/elaphe/",
     test_suite = 'runner',
 )
 
+sys.path.insert(0, abspath('./tests'))
 setup(**setup_params)
