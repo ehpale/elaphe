9c34d659da693e0ebe527d247d4aa0b6bdf68eab
Thomas Grainger <tom@procensus.com>
2017-05-03 17:40:05 +0100

    upgrade BWIPP

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-version='2016-04-21'
+version='2017-04-04'
 curl -fSL https://github.com/bwipp/postscriptbarcode/releases/download/$version/postscriptbarcode-monolithic-package-$version.tgz -o psbc.tgz
 
 tar -zxC ./elaphe/postscriptbarcode/ -f psbc.tgz --strip=2 --no-anchored barcode.ps LICENSE README
